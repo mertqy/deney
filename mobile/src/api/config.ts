a137fffe56486cb9b8f887b8dfa3350cc48c88f9
Mert Aktas <86933998+mertqy@users.noreply.github.com>
2026-02-24 19:20:10 +0300

    .

--- mobile/src/api/config.ts
+++ mobile/src/api/config.ts
@@ -1,5 +1,4 @@
 export const CONFIG = {
     // Üretim aşamasında buradaki URL'yi backend sunucunuzun adresiyle değiştirin.
-    // Örn: 'https://junto-api.onrender.com/api'
-    API_URL: 'http://192.168.1.13:3000/api',
+    API_URL: 'https://deney-d2x5.onrender.com/api',
 };
